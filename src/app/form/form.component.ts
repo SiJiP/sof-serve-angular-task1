97ead9281be5286917222ce32c98f8543b64f87a
Sergiy Petruniv <petruniv.s.i@gmail.com>
2019-07-15 10:11:53 +0300

    add remove for todo list

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm, FormControl } from "@angular/forms";
+import { Container } from "@angular/compiler/src/i18n/i18n_ast";
 
 @Component({
   selector: "app-form",
   templateUrl: "./form.component.html",
   styleUrls: ["./form.component.css"]
 })
+
 export class FormComponent implements OnInit {
   name: FormControl = new FormControl("");
   surname: FormControl = new FormControl("");
@@ -14,15 +16,19 @@ export class FormComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
-
-  changes(): void {
-    if (this.name.value != "" && this.surname.value != "") {
-      this.users.push({
-        name: this.name.value,
-        surname: this.surname.value,
-        email: this.email.value
-      });
-    }
+  onClick(): void {
+    let obj = {
+      name: this.name.value,
+      surname: this.surname.value,
+      email: this.email.value
+    };
+    if (this.name.value != "" && this.surname.value != "") this.users.push(obj);
   }
+
+  onDelete($event) {
+    let index = [...$event.target.parentElement.children].indexOf($event.target);
+    this.users.splice(index, 1);
+  }
+  
+  ngOnInit() {}
 }
